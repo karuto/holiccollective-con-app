72fc7bb0f8cdb964a7964de7832781ff124de446
Vincent Zhang <vzhang@opentable.com>
2026-01-29 10:01:28 -0800

    Added additional art

--- src/components/Art/Art.js
+++ src/components/Art/Art.js
@@ -4,50 +4,21 @@ import art1 from "../../assets/art/art-1.png";
 import art2 from "../../assets/art/art-2.png";
 import art3 from "../../assets/art/art-3.png";
 import art4 from "../../assets/art/art-4.png";
+import art5 from "../../assets/art/art-5.png";
 import Heading from "../Heading/Heading";
 
-function Art() {
-  const content = {
-    images: [
-      {
-        src: art1,
-        alt: "Art showcase",
-        isFullWidth: true,
-      },
-      {
-        src: art2,
-        alt: "Art showcase",
-        isFullWidth: true,
-      },
-      {
-        src: art3,
-        alt: "Art showcase",
-        isFullWidth: true,
-      },
-      {
-        src: art4,
-        alt: "Art showcase",
-        isFullWidth: true,
-      },
-    ],
-  };
+const artImages = [art1, art2, art3, art4, art5];
 
+function Art() {
   return (
     <section className={styles.art}>
       <Heading subtitle="Honkai: Star Rail & Genshin Impact" title="Art Showcase" />
       <div className={styles.art__grid}>
-        {content.images.map((image, index) => (
-          <figure
-            key={index}
-            className={`${styles.art__item} ${
-              image.isFullWidth
-                ? styles.art__item_full
-                : styles.art__item_half
-            }`}
-          >
+        {artImages.map((img, index) => (
+          <figure key={index} className={`${styles.art__item} ${styles.art__item_full}`}>
             <img
-              src={`dist/${image.src}`}
-              alt={image.alt}
+              src={`dist/${img}`}
+              alt="Art showcase"
               className={styles.art__image}
             />
           </figure>
